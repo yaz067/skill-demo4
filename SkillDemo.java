bcbdcbae08d665eb736f0364194bf8e27fb7d093
yaz067 <97631896+yaz067@users.noreply.github.com>
2022-02-03 20:25:01 -0800

    lib + tests

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,1 +1,5 @@
-public class SkillDemo{}
+public class SkillDemo{
+    public static int add(int n ,int m){
+        return n + m;
+    }
+}
---
+++ SkillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void addition() {
+        assertEquals(5, SkillDemo.add(1, 3));
+    }
+}
